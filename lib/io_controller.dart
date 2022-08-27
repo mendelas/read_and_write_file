52f60888c60522f80dbf4d2cd61e244fdf385529
mendelas <kota4610@gmail.com>
2022-08-27 17:37:23 +0900

    third commit

---
+++ lib/io_controller.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
+
+final ioProvider = ChangeNotifierProvider.autoDispose<IoController>(
+      (ref) => IoController(),
+);
+
+
+class IoController extends ChangeNotifier {
+  IoController(){
+    /// 初期化処理をここに書く
+
+    /// コンストラクタで非同期処理をやりたい場合どうすのがいいんでしょう？
+        Future(() async {
+      content = 'ファイルに書き込まれた時間を表示します';
+      final _localPath = await localPath;
+      appPath = '$_localPath/playground/';
+      appDirectory =Directory(appPath);
+
+      /// 新しくディレクトリをつくる
+      await appDirectory.create(recursive: true);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  String appPath='';//変更箇所20220827/初期化した
+  String content='';//変更箇所20220827/初期化した
+  Directory appDirectory = Directory('');//変更箇所20220827/初期化した
+
+  /// ローカルパスの取得
+  Future<String> get localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    notifyListeners();
+    return directory.path;
+  }
+
+  /// 現在時刻の書き込み
+  Future<void> write() async {
+    final file = File('$appPath/test.txt');
+    print('write: ${file.path}');
+    await file.writeAsString(DateTime.now().toString());
+  }
+
+  /// 現在時刻の読み込み
+  Future<void> read() async {
+    final file = File('$appPath/test.txt');
+    content = await file.readAsString();
+    notifyListeners();
+  }
+}
---
+++ lib/io_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+// riverpod を使う場合は忘れずに pubspec.yaml を編集して pub get
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'io_controller.dart';
+
+void main(){//変更箇所20220827
+  runApp(
+    ProviderScope(
+        child: const IoPage(),///https://zenn.dev/junki555/articles/17382e2862c46b
+    ),
+  );
+}
+
+// ページは描画しているだけなので詳しい内容はコントローラーへ GO
+class IoPage extends ConsumerWidget {
+  const IoPage({Key? key}) : super(key: key);
+  static const String title = '外部データの入出力';
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _ioProvider = ref.watch(ioProvider);
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text('アプリのローカルパス: \n${_ioProvider.appPath}'),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: SizedBox(
+                        height: 25,
+                        child: FittedBox(
+                          fit: BoxFit.fitHeight,
+                          child: Text(
+                            _ioProvider.content,
+                          ),
+                        )),
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await _ioProvider.write();
+                        await _ioProvider.read();
+                      },
+                      child: const Text('いまの時間を書き込む'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      );
+  }
+}
